89d7f1b9624979f9f21826792471ede6be965c96
Mihir72999 <111626217+Mihir72999@users.noreply.github.com>
2023-11-24 05:36:02 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import {store , persist }from './state/store'
 
-import { brandmodelSlice } from './state/brandmodelSlice.js'
-import { expandedSlice } from './state/expandedSlice.js'
 import { PersistGate } from 'redux-persist/integration/react';
 
 import AOS from 'aos';
@@ -18,21 +16,17 @@ import Navbar from './Component/Navbar.jsx'
 
 AOS.init()
 
-store.dispatch(brandmodelSlice.endpoints.getBrandmodel.initiate())
-store.dispatch(expandedSlice.endpoints.getProduct.initiate())
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store} >
       <PersistGate persistor={persist} loading={null}>
-        <CounterProvider>
-      <BrowserRouter>
+       <BrowserRouter>
         <Navbar />
         <App />
       </BrowserRouter>
-      </CounterProvider>
-      </PersistGate>
+       </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
